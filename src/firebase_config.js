10b112cb483db6c571ce53dd736be79cddd9d075
Ayushanupam7 <ayushanupamofficial7@gmail.com>
2026-03-27 15:07:59 +0530

    modified:   src/firebase_config.js

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBqsnfXd3Q9kbAaqMRoOECdJY2m36EngAA",
-  authDomain: "personal-chatbot-a5c1f.firebaseapp.com",
-  projectId: "personal-chatbot-a5c1f",
-  storageBucket: "personal-chatbot-a5c1f.firebasestorage.app",
-  messagingSenderId: "116403394849",
-  appId: "1:116403394849:web:3cdc3194063d27f314c660",
-  measurementId: "G-D3P60Q34BR"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
